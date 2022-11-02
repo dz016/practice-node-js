e44548d4ab5736d99ca50ee674cd0d19ba98d625
016_dawood Zargar <dawoodzargar08@gmail.com>
2022-11-02 05:49:38 +0000

    Update 4 files

---
+++ hello.js
@@ -0,0 +1,22 @@
+// program to check if the string is palindrome or not
+const prompt = require("prompt-sync")();
+function checkPalindrome(string) {
+
+    // find the length of a string
+    const len = string.length;
+
+    // loop through half of the string
+    for (let i = 0; i < len / 2; i++) {
+
+        // check if first and last string are same
+        if (string[i] !== string[len - 1 - i]) {
+            return 'It is not a palindrome';
+        }
+    }
+    return 'It is a palindrome';
+}
+const string = prompt("enter a string:");
+
+const value = checkPalindrome(string);
+
+console.log(value);
